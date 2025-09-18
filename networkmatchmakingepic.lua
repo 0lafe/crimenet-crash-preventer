4a17b2818cf8f8ea89af76c092ac78b86ec5f473
0lafe <0lafethehorsescrub@gmail.com>
2025-09-18 10:47:44 -0500

    player count fix and opening lobby fix

--- networkmatchmakingepic.lua
+++ networkmatchmakingepic.lua
@@ -8,7 +8,13 @@ Hooks:PreHook(NetworkMatchMakingEPIC, "is_server_ok", "prevent_crimenet_crash",
 
     -- Invalid difficulty index
     if attributes_list.numbers[2] == nil then
-      attributes_list.numbers[2] = 0
+      attributes_list.numbers[2] = 8
+      _G.CrimenetCrashPreventer.found_issue = true
+    end
+
+    -- Invalid player count
+    if attributes_list.numbers[5] == nil then
+      attributes_list.numbers[5] = 4
       _G.CrimenetCrashPreventer.found_issue = true
     end
   end
